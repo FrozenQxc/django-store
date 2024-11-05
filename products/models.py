99dc1243403ca7078ef6e3ae45e6852b97a21588
FrozenDev <125030768+FrozenQxc@users.noreply.github.com>
2024-11-05 13:55:30 +0300

    create: fuxture

--- products/models.py
+++ products/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-# Create your models here.
 
 class ProductCategory(models.Model):
   name = models.CharField(max_length=128)
@@ -18,7 +17,7 @@ class Product(models.Model):
   quantity = models.PositiveIntegerField(default=0)
   image = models.ImageField(upload_to='products_images')
   
-  category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE) # Привязка к категории 
+  category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE) #!Привязка к категории 
 
   def __str__(self):
     return f'Продукт: {self.name} | Категория: {self.category}'
@@ -33,4 +32,4 @@ class Slider(models.Model):
 @receiver(post_delete, sender=Slider)
 def delete_slider_image(sender, instance, **kwargs):
     if instance.image:
-        instance.image.delete(False)  # False чтобы не сохранять модель
+        instance.image.delete(False)  
