92ec10f15af4601285de9075399d1b7a7e90ceeb
FrozenDev <125030768+FrozenQxc@users.noreply.github.com>
2024-11-22 14:17:03 +0300

    👾feat: pagination

--- products/views.py
+++ products/views.py
@@ -8,12 +8,12 @@ from products.models import Product, ProductCategory, Slider
 def index(request):
   return render(request, 'products/index.html')
 
-def products(request, category_id=None, page=1):
+def products(request, category_id=None, page_number=1):
   products = Product.objects.filter(category_id = category_id) if category_id else Product.objects.all()
 
   per_page = 3
   paginator = Paginator(products, per_page)
-  products_paginator = paginator.page(page)
+  products_paginator = paginator.page(page_number)
    
     
   context = {
